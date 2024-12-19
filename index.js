d7af46a953aec25f87d943ed3ec659671d6528ff
MonangiUpendra <monangiupendra@gmail.com>
2024-12-19 12:13:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
+
 function display(value) {
     document.getElementById("result").value += value;
-}
 
-function clearscreen() {
+function clearDisplay() {
     document.getElementById("result").value = "";
 }
+
 
 function calculate() {
     var p = document.getElementById("result").value;
     try {
+        if (p.includes("/0")) {
+            throw new Error("Division by zero");
+        }
         var q = Function(`'use strict'; return (${p})`)();
         document.getElementById("result").value = q;
     } catch (e) {
         document.getElementById("result").value = "Error";
-    }
-}
-
-function remove() {
-    var display = document.getElementById("result");
-    if (display.value === "Error") {
-        display.value = ""; //
-    } else {
-        display.value = display.value.slice(0, -1);
     }
 }
