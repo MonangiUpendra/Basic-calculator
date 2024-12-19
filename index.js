be3ca1c21c4ddb06ca37409205147385731c2cb4
MonangiUpendra <monangiupendra@gmail.com>
2024-12-19 11:13:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function display(value){
-    document.getElementById("result").value+=value
-
-}
-function clearscreen(){
-    document.getElementById("result").value=""
+function display(value) {
+    document.getElementById("result").value += value;
 }
 
-function calculate(){
-    var p=document.getElementById("result").value
+function clearscreen() {
+    document.getElementById("result").value = "";
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
     try {
         var q = Function(`'use strict'; return (${p})`)();
         document.getElementById("result").value = q;
-    } 
-    catch (e) {
+    } catch (e) {
         document.getElementById("result").value = "Error";
     }
 }
+
 function remove() {
     var display = document.getElementById("result");
     if (display.value === "Error") {
-        display.value = "";
+        display.value = ""; //
     } else {
         display.value = display.value.slice(0, -1);
     }
